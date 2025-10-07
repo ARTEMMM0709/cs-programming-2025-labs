ac7173f6f06611dd658a7c710019915b10dbcf0e
ARTEMMM0709 <artemvoron1208@gmail.com>
2025-10-07 12:25:51 +1000

    vghhg

---
+++ labs/lab1/5.py
@@ -0,0 +1,5 @@
+width = float(input("Задание 5: Введите ширину прямоугольника: "))
+height = float(input("Задание 5: Введите высоту прямоугольника: "))
+perimeter = 2 * (width + height)
+area = width * height
+print(f"Площадь: {area}, Периметр: {perimeter}")
