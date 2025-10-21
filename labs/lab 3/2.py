5486006caac521b1cf2389903183105bee7d27a8
ARTEMMM0709 <artemvoron1208@gmail.com>
2025-10-21 12:41:45 +1000

    2

--- labs/lab 3/2.py
+++ labs/lab 3/2.py
@@ -1,10 +1,3 @@
-number = int(input("Введите число от 1 до 9: "))
-
-if 1 <= number <= 9:
-    print(f"Таблица умножения для числа {number}:")
-    for i in range(1, 11):
-        result = number * i
-        print(f"{number} * {i} = {result}")
-else:
-    print("Ошибка! Введите число от 1 до 9.")
-   
+num = int(input("Введите число от 1 до 9: "))
+for i in range(1, 11):
+    print(f"{num} x {i} = {num * i}")
