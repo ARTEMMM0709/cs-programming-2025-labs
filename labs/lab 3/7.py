37710b7e5bcc66e59ddbbe7f18773a122ef8a4e8
ARTEMMM0709 <artemvoron1208@gmail.com>
2025-10-21 12:47:21 +1000

    7

--- labs/lab 3/7.py
+++ labs/lab 3/7.py
@@ -1,7 +1,5 @@
-text = input("Введите строку: ")
-
-result = ""
-for i, char in enumerate(text, 1):
-    result += char + str(i)
-
-print(f"Результат: {result}")
+s = input("Введите строку: ")
+new_s = ""
+for i in range(len(s)):
+    new_s += s[i] + str(i + 1)
+print(new_s)
