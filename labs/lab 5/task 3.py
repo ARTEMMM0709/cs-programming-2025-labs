069f1e00a3d76a4c33f1e2f3efe0a3c44bbe82ad
ARTEMMM0709 <artemvoron1208@gmail.com>
2025-12-21 18:28:10 +1000

    3

---
+++ labs/lab 5/task 3.py
@@ -0,0 +1,11 @@
+# Создаем список
+numbers = [12, 45, 23, 67, 34]
+
+# Находим максимальное число
+max_num = max(numbers)
+# Находим длину списка
+length = len(numbers)
+# Делим максимальное на длину
+result = max_num / length
+
+print(result)
