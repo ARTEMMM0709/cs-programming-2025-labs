34156b8d5441e4d1e3e3a7fb9fa0d21d8e3dcf30
ARTEMMM0709 <artemvoron1208@gmail.com>
2026-01-19 18:46:47 +1000

    csdc

--- labs/lab 3/3.py
+++ labs/lab 3/3.py
@@ -1,2 +1,14 @@
-for i in range(0, 101, 3):
-    print(i)
+a = input('').split()
+cnt_colors = 1
+x = int(a[0])
+y = int(a[1])
+r = int(a[2])
+if y <= r or x <= r:
+    cnt_colors += 1
+if x < r:
+    cnt_colors += 1
+if x ** 2 + y  **2 < r ** 2:
+    cnt_colors += 1
+if y < r:
+    cnt_colors += 1
+print(cnt_colors)
